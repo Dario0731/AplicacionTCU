d67bb1b8e21f7b55f9214cc3d19f23bdf6af3207
Dario Zamora <82007761+Dario0731@users.noreply.github.com>
2024-05-23 15:27:30 -0600

    update user
    s

--- App/Model/User.php
+++ App/Model/User.php
@@ -21,6 +21,38 @@ class User {
         $this->password = $password;
         $this->type = $type;
     }
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function getPassword() {
+        return $this->password;
+    }
+
+    public function getType() {
+        return $this->type;
+    }
+
+    public function setId($id): void {
+        $this->id = $id;
+    }
+
+    public function setEmail($email): void {
+        $this->email = $email;
+    }
+
+    public function setPassword($password): void {
+        $this->password = $password;
+    }
+
+    public function setType($type): void {
+        $this->type = $type;
+    }
+
 
     
 }
