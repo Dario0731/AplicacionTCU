6699dfd09fc4c7e04a18c3e6956fa06ef7200026
Dario Zamora <82007761+Dario0731@users.noreply.github.com>
2024-05-31 14:56:07 -0600

    LOGO
    LOGO

--- App/View/Home/index.view.php
+++ App/View/Home/index.view.php
@@ -8,7 +8,7 @@
         </p>
     </div>
     <div class="text-center p-5">
-        <img src="../Public/Assets/img/home-image.svg" alt="Imagen de inicio">
+        <img src="<?=CONFIG['assets']?>img/home-image.svg" alt="Imagen de inicio">
     </div>
     <div class="container">
         <article class="p-2">
@@ -19,3 +19,5 @@
 </main>
 
 <?php include(CONFIG['public_path'].'footer.php'); ?>
+
+http://localhost/AplicacionTCU/Public/Assets/img/home-image.svg
