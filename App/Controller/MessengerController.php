d900e007b67cebda892edd573aafb684e268a5d1
Dario Zamora <82007761+Dario0731@users.noreply.github.com>
2024-07-14 17:38:59 -0600

    Mensaje a cliente
    Mensaje a cliente

---
+++ App/Controller/MessengerController.php
@@ -0,0 +1,52 @@
+<?php
+require_once("Lib/Core/Controller.php");
+require_once(CONFIG["repository_path"] . "ClientRepository.php");
+require_once(CONFIG["repository_path"] . "CoachRepository.php");
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+/**
+ * Description of MessengerController
+ *
+ * @author 50685
+ */
+class MessengerController extends Controller  {
+    //put your code here
+    
+    public function clientMessage(){
+              if (!isset($_SESSION['user']) || !isset($_SESSION['user']['email'])) {
+// Si el usuario no ha iniciado sesión, redirigirlo a la página de inicio de sesión
+            $this->redirect("/authentication/login");
+        }
+        $client = new ClientRepository();
+        $coach = new CoachRepository();
+        $email = $_SESSION['user']['email'];
+
+        try {
+            $coachsByEmail = $coach->getByEmail($email);
+            $clientList = $client->getByCoach($coachsByEmail['id']);
+            //     $clientList = $client->getAll();
+
+            if (empty($clientList)) {
+                $info = [
+                    'type' => 'error',
+                    'title' => 'No existen clientes',
+                    'text' => 'No hay clientes para mostrar'
+                ];
+            } else {
+                viewbag("clientes", $clientList);
+            }
+        } catch (Exception $ex) {
+            $info = [
+                'type' => 'error',
+                'title' => 'Error al recuperar los datos',
+                'text' => 'Error en la carga de datos.'
+            ];
+            $_SESSION['redirect-info'] = $info;
+        }
+
+        return View();
+    }
+}
---
+++ App/View/Messenger/clientMessage.view.php
@@ -0,0 +1,56 @@
+<?php include(CONFIG['public_path'] . 'header.admin.php'); ?>
+
+<div class="container p-4">
+    <div class="" style="height: 100%;">
+        <form action="<?= route('Admin', 'saveSelectedClient') ?>" method="POST">
+            <div class="form-group">
+                <label for="selectClient" class="text-white">Seleccionar Cliente:</label>
+                <select class="form-control" id="selectClient" name="client_id" required>
+                    <option value="" selected disabled>Seleccione un cliente</option>
+                    <?php if (is_array(viewbag("clientes"))) : ?>
+                        <?php foreach (viewbag("clientes") as $client) : ?>
+                            <option value="<?= $client['id'] ?>">
+                                <?= htmlspecialchars($client['name']) . ' ' . htmlspecialchars($client['last_name']) ?>
+                            </option>
+                        <?php endforeach; ?>
+                    <?php else : ?>
+                        <option value="" disabled>No hay clientes disponibles</option>
+                    <?php endif; ?>
+                </select>
+                                    <div class="form-group py-1">
+                        <label for="clients_comments">Mensaje para el cliente:</label>
+                        <textarea class="form-control text-center" id="clients_comments" name="clients_comments" required rows="2"></textarea>
+                    </div>
+            </div>
+            <div class="form-group text-center">
+                <button type="submit" class="btn btn-primary">Enviar mensaje</button>
+            </div>
+            <br>
+                        <br>
+                        <div class="form-group text-center">
+                                <label for="clients_comments">Si quiere seleccionar un grupo de clientes presione aquí:</label>
+                      <div class="text-end"><a class="btn btn-primary" href="<?= route('Messenger', 'groupMessage') ?>">Ir a grupos</a></div>
+            </div>
+        </form>
+    </div>
+</div>
+
+<?php include(CONFIG['public_path'] . 'footer.php'); ?>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        <?php if (isset($_SESSION['redirect-info'])) : ?>
+            Swal.fire({
+                icon: '<?php echo $_SESSION['redirect-info']['type']; ?>',
+                title: '<?php echo $_SESSION['redirect-info']['title']; ?>',
+                text: '<?php echo $_SESSION['redirect-info']['text']; ?>',
+                background: 'linear-gradient(to bottom, #011242, #001136)',
+                color: '#fff',
+                iconColor: '#fff',
+                confirmButtonColor: '#3085d6'
+            });
+            <?php unset($_SESSION['redirect-info']); ?>
+        <?php endif; ?>
+    });
+</script>
